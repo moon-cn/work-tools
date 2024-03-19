653c07d8fe4bae45d58f454cefb432e1de9c005f
jiangtao <410518072@qq.com>
2024-03-19 20:28:21 +0800

    增加一些想法

---
+++ main/src/main/java/cn/moon/tool/img/ImgCompressTool.java
@@ -0,0 +1,17 @@
+package cn.moon.tool.img;
+
+import cn.moon.WorkTool;
+
+import javax.swing.*;
+
+public class ImgCompressTool implements WorkTool {
+    @Override
+    public String getName() {
+        return "图片-压缩";
+    }
+
+    @Override
+    public void onToolBtnClick(JPanel wrapPanel) {
+
+    }
+}
